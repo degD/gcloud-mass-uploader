b27c602f548015b3ca562c6e91c2fc89f4d2b3d9
degD <108623049+degD@users.noreply.github.com>
2024-11-11 12:37:07 +0300

    add rclone functionality

--- main.py
+++ main.py
@@ -1,18 +1,22 @@
 from pathlib import Path
+import subprocess
 import time
+from progress.bar import Bar
 import json
 
 
-def upload_file(filepath):
+def mock_upload_file(filepath, remote):
     "Mock function of file upload"
     time.sleep(1)
-    print(f"{filepath} uploaded successfully!")
 
+def upload_file(filepath, remote_name):
+    "Real function for uploading"
+    if subprocess.run(["rclone", "copy", filepath, f"{remote_name}:upload"], capture_output=True).returncode != 0:
+        raise Exception
 
 def save_files_list(files_list, savefile):
     "Serialize and save folder structure"
     json.dump(files_list, savefile)
-
 
 def save_index(index, savefile):
     "Serialize and save progress"
@@ -20,8 +24,9 @@ def save_index(index, savefile):
     savefile.write(str(index))
 
 
-
 if __name__ == "__main__":
+
+    remote = "gphotos"
 
     # Folder structure not indexed yet. Do the indexing and save the result.
     if not Path("files.json").exists():
@@ -46,18 +51,30 @@ if __name__ == "__main__":
         # No progress (index) file. Start from 0.
         if not Path("progress").exists():
             i = 0
+            print(i)
 
         # Create a new progress file.
         else:
             with open("progress") as p:
-                i = int(p.read(1))
-            progressfp = open("progress", "w")
+                i = int("".join(p.readline()))
+                print(i)
     
     # Continue uploading files from where it left.
-    while i < len(files):
-        filepath = files[i]
-        print(f"{i} -) ", end="")
-        upload_file(filepath)
-        save_index(i, progressfp)
-        i += 1
+    print(i)
+    if i != -1:
+        progressfp = open("progress", "w")
+        with Bar("Uploading", index=i, max=len(files), suffix = f"at file %(index)d") as bar:
+            while i < len(files):
+                filepath = files[i]
+                try:
+                    upload_file(filepath, remote)
+                except:
+                    print(f"\n\tERROR: Either file is not a media, or it's corrupted. Skipping file: {filepath}...\n")
+                save_index(i, progressfp)
+                i += 1
+                bar.next()
 
+        i = -1
+        save_index(i, progressfp)
+
+    print("Upload completed!")
