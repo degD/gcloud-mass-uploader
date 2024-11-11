aee8b1aaa1de4d8b6091f0fb9b97223f42ef60d3
degD <108623049+degD@users.noreply.github.com>
2024-11-11 13:01:55 +0300

    complete main program mostly

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ def mock_upload_file(filepath, remote):
 def upload_file(filepath, remote_name):
     "Real function for uploading"
     if subprocess.run(["rclone", "copy", filepath, f"{remote_name}:upload"], capture_output=True).returncode != 0:
-        raise Exception
+        raise FileNotFoundError
 
 def save_files_list(files_list, savefile):
     "Serialize and save folder structure"
@@ -32,8 +32,9 @@ if __name__ == "__main__":
     if not Path("files.json").exists():
 
         i = 0
-        # dir = input("Enter mass-upload directory: ")
-        dir = "testdir"
+        skipped = []
+        dir = input("Enter mass-upload directory: ")
+        # dir = "testdir"
         try:
             files = list(str(p) for p in Path(dir).rglob("*") if Path.is_file(p))
             with open("files.json", "w") as fp:
@@ -48,33 +49,52 @@ if __name__ == "__main__":
         with open("files.json", "r") as fp:
             files = json.load(fp)
 
+        # No info about skipped files.
+        if not Path("skipped_files.json").exists():
+            skipped = []
+        
+        # There exist info.
+        else:
+            with open("skipped_files.json", "r") as fp:
+                skipped = json.load(fp)
+
         # No progress (index) file. Start from 0.
         if not Path("progress").exists():
             i = 0
-            print(i)
+            skipped = []
 
-        # Create a new progress file.
+        # Read index from progress file.
         else:
             with open("progress") as p:
-                i = int("".join(p.readline()))
-                print(i)
-    
+                try:
+                    i = int("".join(p.readline()))
+                except ValueError:
+                    i = 0
+
+        # If progress (index) is 0, remove skipped_files.json
+        if i == 0:
+            with open("skipped_files.json", "w") as fp:
+                skipped = []
+
     # Continue uploading files from where it left.
-    print(i)
     if i != -1:
         progressfp = open("progress", "w")
-        with Bar("Uploading", index=i, max=len(files), suffix = f"at file %(index)d") as bar:
+        with Bar("Uploading", index=i, max=len(files), suffix = "at file %(index)d/%(max)d") as bar:
             while i < len(files):
                 filepath = files[i]
                 try:
                     upload_file(filepath, remote)
-                except:
+                except FileNotFoundError:
                     print(f"\n\tERROR: Either file is not a media, or it's corrupted. Skipping file: {filepath}...\n")
+                    skipped.append(filepath)
+                    with open("skipped_files.json", "w") as sf:
+                        json.dump(skipped, sf)
                 save_index(i, progressfp)
                 i += 1
                 bar.next()
 
         i = -1
+        progressfp.close()
         save_index(i, progressfp)
 
     print("Upload completed!")
